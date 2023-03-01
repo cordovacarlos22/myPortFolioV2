ec87713cc13094f725567e62951dade1e28e21f2
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-03-01 08:08:01 -0800

    add babel

---
+++ my-personal-portfolio/schemas/testimonials.js
@@ -0,0 +1,15 @@
+// schemas/tesmonials.js
+
+export default {
+  name:'testimonials',
+  title:'Testimonials',
+  type:'document',
+  fields:[
+    {
+      name: 'name',
+      title:'name',
+      
+    },
+
+  ],
+}
