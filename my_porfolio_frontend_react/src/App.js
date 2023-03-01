cf16dab0403f6adc221c7d479649258d449fe070
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-03-01 09:53:01 -0800

    update on app.js

--- my_porfolio_frontend_react/src/App.js
+++ my_porfolio_frontend_react/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import {About,Footer,Header,Skills,Testimonial,Work} from './container';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
+import './App.scss';
+
 const App = () => {
   return (
     <div className="app">
-      <Navbar/>
-      <Header/>
-      <About/>
-      <Work/>
-      <Skills/>
-      <Testimonial/>
-      <Footer/>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
 
     </div>
   );
