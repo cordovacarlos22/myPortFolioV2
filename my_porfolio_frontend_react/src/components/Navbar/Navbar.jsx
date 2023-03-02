6b4a1f1f088c71e54ecc6a4c219033338f099abe
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-03-02 07:47:13 -0800

    nav bar is done

--- my_porfolio_frontend_react/src/components/Navbar/Navbar.jsx
+++ my_porfolio_frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.scss';
-import { images } from '../../constans'
+import { images } from '../../constans';
+import { HiXCircle, HiMenu } from 'react-icons/hi'
+import { motion } from 'framer-motion';
+
 const Navbar = () => {
+
+  const [Toggle, setToggle] = useState(false);
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img className='app__navbar-logo'  id="logo" src={images.logo} alt="logo" />
+        <img className='app__navbar-logo' id="logo" src={images.logo} alt="logo" />
       </div>
       <ul className='app__navbar-links'>
-        
+
         {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className='app__flex p-text' key={`link-${item}`}>
             <div />
@@ -16,6 +22,30 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div className='app__navbar-menu'>
+        <HiMenu onClick={() => setToggle(true)} />
+        {
+          Toggle && (
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.7,ease:'easeOut' }}
+              animate={{ x: -5 }}
+            >
+              
+              <HiXCircle onClick={() => setToggle(false)} />
+              <ul>
+
+                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                  <li  key={item}>
+                    <a href={`#${item}`} onClick={() => setToggle(false)} >{item}</a>
+                  </li>
+                ))}
+              </ul>
+            </motion.div>
+            
+          )
+        }
+      </div>
     </nav>
   );
 };
