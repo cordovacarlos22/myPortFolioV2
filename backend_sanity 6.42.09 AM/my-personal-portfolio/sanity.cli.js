7772fc44288f204165a276a911511cc4fedec0b6
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-03-02 06:00:59 -0800

    navbar on big screens done

---
+++ backend_sanity 6.42.09 AM/my-personal-portfolio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'sf71yav5',
+    dataset: 'production'
+  }
+})
--- my_porfolio_frontend_react/src/components/Navbar/Navbar.jsx
+++ my_porfolio_frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Navbar.scss';
-
+import { images } from '../../constans'
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img className='app__navbar-logo'  id="logo" src={images.logo} alt="logo" />
+      </div>
+      <ul className='app__navbar-links'>
+        
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
