251b87a79c710874041f2871dd358ddb50bf9171
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-03-04 12:07:21 -0800

    update on navbar, working on header section

--- my_porfolio_frontend_react/src/container/Header/Header.jsx
+++ my_porfolio_frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
 import './Header.scss'
+import { motion } from 'framer-motion'
+import { images } from '../../constans'
+
 const Header = () => {
+
+  const scaleVariants = {
+    whileInView: {
+      scale: [0, 1],
+      opacity: [0, 1],
+      transition: {
+        duration: 1,
+        default: { ease: "linear" },
+        delay: .5
+      }
+
+    }
+  }
   return (
-    <div>
-      header
+    <div  id="home" className='app__header app__flex'>
+      <motion.div
+        whileInView={{
+          x: [-100, 0], opacity: [0, 1],
+          // rotate: [270, 0, 0, 0, 0],
+          // borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        }}
+        transition={{ duration: 1 }}
+
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'>Hello I'm </p>
+              {/* <h1 className='head-text'>Carlos</h1> */}
+            </div>
+          </div>
+        </div>
+        <div className='tag-cmp app__flex'>
+          <p className='p-text'>{`<Full Stack  Web Developer `}</p>
+          <span>👨‍💻</span>
+          <motion.div
+            animate={{ x: [0, 0], opacity: [-100, 10] }}
+            transition={{ duration: .8, repeat: Infinity, ease: "easeOut" }}
+          >
+            <span className='p-text' >{` |  `}</span>
+          </motion.div>
+          {" "}
+          <span className="p-text">{`/> `}</span>
+        </div>
+
+      </motion.div>
+      <motion.div
+        whileInView={{
+          opacity: [0, 5]
+        }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img  id="profile" src={images.profile} alt="profile_bg"></img>
+        <motion.img
+          whileInView={{
+            scale: [0, 1]
+          }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circle"
+      >
+       {[images.flutter,images.redux,images.sass].map((circle,index)=>(
+        <div className='circle-cmp app__flex' key={`circle-${index}`}>
+          <img src={circle} alt="circle" />
+        </div>
+       ))}
+      </motion.div>
     </div>
   )
 }
