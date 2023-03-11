53550a1a4f883cdb9dbac4f957e304a3f1d2124e
CARLOS CORDOVA <cordovamanuel22@icloud.com>
2023-03-10 20:11:16 -0800

    Update client.js

--- my_porfolio_frontend_react/src/client.js
+++ my_porfolio_frontend_react/src/client.js
@@ -2,11 +2,11 @@ import { createClient} from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: process.env.REACT_APP_ID,
+  projectId: "iyy0n0b6",
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token:'skMCCaesKjWjnfAbI2C0YTCXPDB5ch3ggeHO8VtCI7cABwbZ8NaaPHdOUSzvlKW1UwfydLJff43uEG5VKeKeGve1jr3WVcOJJ2mlwqSHZOoV7tvJjnTjD5w8FtkICpCXnlQvvebEh7frPDPIf9lGYgDv4e9zZ5OmT48hsQFJTTFD2CnVUGg6',
   ignoreBrowserTokenWarning: true
 });
 
